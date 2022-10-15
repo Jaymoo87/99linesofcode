c188613f4ac9397d337bbaf6bffb01e4efa52f87
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2022-10-14 21:13:13 -0500

    99filesofcode project complete

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
 
-let friends = ['Jamie', 'Jen', 'Sequoya', 'Jeff', 'Logan']
+const friends = ['Jamie', 'Jen', 'Sequoya', 'Jeff', 'Logan'];
 
-    
+
+function lines() {
+    let lastLine;
+    let lastWords;
+}
+
+for (i = 0, a = 0; i == 0; ++a) {
+    if (a == friends.length) {
+        break;
+    }
+    else {
+        console.log(friends[a] + ":");
+
+
+
+        for (i = 99; i >= 1; --i) {
+
+
+            if (i == 1) {
+
+                lastLine = "line of code";
+                LastWords = "no more lines of code in the file "
+                console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[a] + " stikes one out, clears it all out, " + lastWords);
+
+            } else {
+
+
+                lastLine = " lines of code";
+                lastWords = i - 1 + " lines of code in the file";
+                console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[a] + " stikes one out, clears it all out, " + lastWords);
+
+            }
+        }
+
+
+    }
+
+
+}
+
+
+
+lines();
